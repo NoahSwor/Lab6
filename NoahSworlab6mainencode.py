7556a1936bb0684ce9a2ff52dad4024b99fc3a46
naraujo123 <149020369+naraujo123@users.noreply.github.com>
2023-10-25 23:02:40 -0400

    Add files via upload

--- NoahSworlab6mainencode.py
+++ NoahSworlab6mainencode.py
@@ -10,11 +10,33 @@ def main():
     if operation == "1":
         encode()
     # elif operation == 2:
-        # decode()
+
 def encode():
     password1 = input('Please enter your password to encode: ')
     print('Your password has been encoded and stored!')
     main()
-# def decode():
-     #  code for partner to add
+  # Neils decode
+def decode(user_pw):
+    key = {
+            3: 3,
+            4: 4,
+            5: 5,
+            6: 6,
+            7: 7,
+            8: 8,
+            9: 9,
+            10: 0,
+            11: 1,
+            12: 2
+    }
+    global encoded_str_pw
+    for i in range(8):
+        encoded_user_pw.append(key[(int(user_pw[i]) + 3)])
+        encoded_digit = str(encoded_user_pw[i])
+        encoded_str_pw = encoded_str_pw + encoded_digit
+        return encoded_str_pw
+
+    encoded_user_pw = []
+    encoded_str_pw = ''
+
 main()
